fa8311b91e289ce300bda734adea2d7d4449420b
zeijier <divcssphp@gmail.com>
2018-03-09 13:29:16 +0800

    add generator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (app()->environment() == 'local' || app()->environment() == 'testing') {
+
+            $this->app->register(\Summerblue\Generator\GeneratorsServiceProvider::class);
+
+        }
     }
 }
