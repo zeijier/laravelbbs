cdf27861441b7d582434e45a8dc417f34f80c305
height <divcssphp@gmail.com>
2018-04-20 14:22:05 +0800

    admin can manage site settings

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
     <meta name="csrf-token" content="{{csrf_token()}}">
     {{--@yield('title', 'LaraBBS') 继承此模板的页面，如果没有定制 title 区域的话，就会自动使用第二个参数 LaraBBS 作为标题前缀。--}}
     <title>@yield('title','laravelBBS')--!!</title>
-    <meta name="description" content="@yield('description','laravelBBS 爱好者社区')">
+    <meta name="description" content="@yield('description',setting('seo_description','laravelBBS 爱好者社区'))">
+    <meta name="keyword" content="@yield('keyword', setting('seo_keyword', 'LaraBBS,社区,论坛,开发者论坛'))" />
     <link href="{{asset('css/app.css')}}" rel="stylesheet">
     @yield('styles')
 </head>
