3433981bc3a745a9c5971cdf952288e289a383ef
height <divcssphp@gmail.com>
2018-05-19 23:12:39 +0800

    后台修复

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -13,7 +13,7 @@ class PageController extends Controller
         //如果当前用户有权限访问后台，直接跳转访问
         // 因为administrator中permission是一个函数，里面得到的是一个对象，所有加() 得到值
         if (config('administrator.permission')()){
-            return redirect(url(config('admin')),302);
+            return redirect(url(config('administrator.uri')),302);
         }
         //否则使用视图
         return view('pages.permission_denied');
