7683b17dd192c50f9d26c6083f5692ae442a586f
height <divcssphp@gmail.com>
2018-05-19 17:20:52 +0800

    add socialiteproviders

--- routes/api.php
+++ routes/api.php
@@ -42,8 +42,11 @@ $api->version('v1',[
 // 用户注册
         $api->post('users', 'UsersController@store')
             ->name('api.users.store');
-        });
 //  图片验证码
         $api->post('captchas','CaptchasController@store')
             ->name('api.captchas.store');
+
+
+        });
+
 });
